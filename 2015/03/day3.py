d0a36312276a9d93f636828e4d0746903575e290
Parker Ellertson <pellertson@firemail.cc>
2024-07-15 20:08:19 -0700

    Completed 2015/03 part 2 in python3

--- 2015/03/day3.py
+++ 2015/03/day3.py
@@ -1,7 +1,7 @@
-def part1(path: str) -> int:
+def deliver(path: str) -> set[str]:
     x = y = 0
-    p: dict[str, int] = {}
-    p["0,0"] = 1
+    p: dict[str, bool] = {}
+    p["0,0"] = True
 
     for house in path:
         if house == ">":
@@ -16,13 +16,22 @@ def part1(path: str) -> int:
             # probably don't need this but it's always good to be thorough
             raise Exception("we done screwed up")   
 
-        key = str(x) + ',' + str(y)
-        try:
-            p[key] += 1
-        except KeyError:
-            p[key] = 1
+        p[str(x) + ',' + str(y)] = True
 
-    return len(p)
+    return set(p.keys())
+
+def part1(path: str) -> int:
+    return len(deliver(path))
+
+def part2(path:str) -> int:
+    a = b = ""
+    for i, h in enumerate(path):
+        if i % 2 == 0:
+            a += h
+        else:
+            b += h
+    #print(deliver(a) + deliver(b))
+    return len(deliver(a) | deliver(b))
 
 def test1() -> bool:
     tests = (
@@ -32,6 +41,14 @@ def test1() -> bool:
             )
     return all(check == part1(test) for test, check in tests)
 
+def test2() -> bool:
+    tests = (
+            ("^v", 3),
+            ("^>v<", 3),
+            ("^v^v^v^v^v", 11),
+            )
+    return all(check == part2(test) for test, check in tests)
+
 def main():
     file = open("input.txt")
     path = file.read()
@@ -39,6 +56,8 @@ def main():
 
     assert test1()
     print(part1(path))
+    assert test2()
+    print(part2(path))
 
 if __name__ == "__main__":
     main()
