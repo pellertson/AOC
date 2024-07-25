2c3600994bba0a1bd0b9518f829d85d4efce0e27
Parker Ellertson <pellertson@firemail.cc>
2024-07-25 02:25:04 -0700

    Finished part 2 of 2015/08 in python 3

--- 2015/08/day8.py
+++ 2015/08/day8.py
@@ -1,6 +1,6 @@
 import fileinput
 
-def parse(file: str) -> int:
+def parse1(file: str) -> int:
     a = b = 0
     s = ""
     with open(file) as f:
@@ -15,6 +15,30 @@ def parse(file: str) -> int:
     
     return a - b
 
+
+def parse2(file: str) -> int:
+    a = b = 0
+    s = '"'
+    with open(file) as f:
+        while (ch := f.read(1)) != '':
+            match ch:
+                case '\n':
+                    s += '"'
+                    a += len(s)
+                    b += len(eval(s))
+                    s = '"'
+                case '"':
+                    s += '\\"'
+                case '\\':
+                    s += '\\\\'
+                case _:
+                    s += ch
+    
+    return a - b
+    
 if __name__ == "__main__":
-    assert 12 == parse("test.txt")
-    print(parse("input.txt"))
+    assert 12 == parse1("test.txt")
+    print(parse1("input.txt"))
+    
+    assert 19 == parse2("test.txt")
+    print(parse2("input.txt"))
